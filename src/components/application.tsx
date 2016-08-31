e01a49c3328f5016346a40838ce71c097822d211
kiresuah <kiresuah@gmail.com>
2016-08-30 22:04:25 -0400

    Implemented redux

--- src/components/application.tsx
+++ src/components/application.tsx
@@ -3,12 +3,29 @@ if (process.env.BROWSER) {
 }
 
 import * as React from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
-export default class Application extends React.Component<{}, {}> {
+export interface IApplicationProps { params: {}; route: {}; }
+
+class Application extends React.Component<{}, {}> {
   public render() {
     return (
-      <div className="application-container">Go to /hello to see react router working</div>
+      <div className="application-container">Go to `/hello/"your name"/` to see react router working</div>
     );
   }
 
 }
+
+function mapStateToProps(state: any) {
+  return {
+    hierarchy: state.appInfo.hierarchy,
+    user: state.user,
+  };
+}
+
+function mapDispatchToProps(dispatch: any) {
+    return bindActionCreators({}, dispatch);
+}
+
+export default connect<{}, {}, IApplicationProps> (mapStateToProps, mapDispatchToProps)(Application);
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { Router, browserHistory } from "react-router";
+import { applyMiddleware, createStore } from "redux";
+import promise from "redux-promise";
 
+import reducers from "./reducers/";
 import router from "./routes.tsx";
 
-ReactDOM.render(<Router history={browserHistory}>{router}</Router>,
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router history={browserHistory}>{router}</Router>
+  </Provider>,
 document.getElementById("react-container"));
